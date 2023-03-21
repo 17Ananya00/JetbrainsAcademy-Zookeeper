40d88d488ac0fba3efba255e7afebc477d105d8b
Ananya <emmelia17.rogers@gmail.com>
2023-03-21 17:54:29 +0530

    Initial commit

---
+++ src/Stage1.java
@@ -0,0 +1,9 @@
+public class Stage1 {
+    public static void main(String[] args) {
+        System.out.println("I love animals!\n" +
+                "Let's check on the animals...\n" +
+                "The deer looks fine.\n" +
+                "The bat looks happy.\n" +
+                "The lion looks healthy.");
+    }
+}
